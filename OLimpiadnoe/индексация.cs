8c95199f31407838fc6fdcd835af308ad4d99687
AverDroz <95124055+AverDroz@users.noreply.github.com>
2023-10-16 13:27:35 +0600

    Add files via upload

---
+++ OLimpiadnoe/индексация.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProductionIntensification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Чтение данных из файла
+            string startDateString = Console.ReadLine();
+            string endDateString = Console.ReadLine();
+            DateTime startDate = DateTime.ParseExact(startDateString, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(endDateString, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            int initialProduction = Convert.ToInt32(Console.ReadLine());
+
+            // Вычисление разницы в днях
+            TimeSpan diff = endDate - startDate;
+            int days = diff.Days + 1;
+
+            // Расчет суммарного объема продукции
+            int totalProduction = initialProduction;
+            for (int i = 1; i < days; i++)
+            {
+                totalProduction += initialProduction + i;
+            }
+
+            // Вывод результата
+            Console.WriteLine(totalProduction);
+        }
+    }
+}
